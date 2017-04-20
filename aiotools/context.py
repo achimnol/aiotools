ac81f9faae39c3368c4e4f24846278b6a75c25f6
Joongi Kim <joongi@lablup.com>
2017-04-21 00:32:10 +0900

    Add abstract types and AsyncContextGroup.
     * Consulted contextlib.py in the standard library.
    
     * Also improve the code coverage.
    
     * Rename "AsyncGenContextManager" to "AsyncContextManager".

--- aiotools/context.py
+++ aiotools/context.py
@@ -1,37 +1,80 @@
+import abc
 import asyncio
 import functools
 import inspect
+from typing import Any, Callable, Iterable, Optional
 import sys
 
-__all__ = ('AsyncGenContextManager', 'async_ctx_manager', 'actxmgr')
+__all__ = (
+    'AsyncContextManager', 'async_ctx_manager', 'actxmgr',
+    'AsyncContextDecorator', 'actxdecorator',
+    'AsyncContextGroup', 'actxgroup',
+)
 
 
-class AsyncGenContextManager:
+class AbstractAsyncContextManager(abc.ABC):
 
-    def __init__(self, func, args, kwargs):
+    async def __aenter__(self):
+        return self  # pragma: no cover
+
+    @abc.abstractmethod
+    async def __aexit__(self, exc_type, exc_value, tb):
+        return None  # pragma: no cover
+
+    @classmethod
+    def __subclasshook__(cls, C):
+        if cls is AbstractAsyncContextManager:
+            if (any('__aenter__' in B.__dict__ for B in C.__mro__) and
+                any('__aexit__' in B.__dict__ for B in C.__mro__)):
+                return True
+        return NotImplemented
+
+
+class AsyncContextDecorator:
+
+    def _recreate_cm(self):
+        return self
+
+    def __call__(self, func):
+        @functools.wraps(func)
+        async def inner(*args, **kwargs):
+            async with self._recreate_cm():
+                return (await func(*args, **kwargs))
+        return inner
+
+
+class AsyncContextManager(AsyncContextDecorator, AbstractAsyncContextManager):
+
+    def __init__(self, func: Callable[..., Any], args, kwargs):
         if not inspect.isasyncgenfunction(func):
-            raise RuntimeError('The context function must be an async-generator.')
+            raise RuntimeError('Context manager function must be async-generator')
+        self._agen = func(*args, **kwargs)
         self.func = func
         self.args = args
         self.kwargs = kwargs
 
+    def _recreate_cm(self):
+        return self.__class__(self.func, self.args, self.kwargs)
+
     async def __aenter__(self):
-        self.agen = self.func(*self.args, **self.kwargs)
-        return (await self.agen.__anext__())
+        try:
+            return (await self._agen.__anext__())
+        except StopAsyncIteration:
+            raise RuntimeError("async-generator didn't yield") from None
 
     async def __aexit__(self, exc_type, exc_value, tb):
         if exc_type is None:
             try:
-                await self.agen.__anext__()
+                await self._agen.__anext__()
             except StopAsyncIteration:
                 return
             else:
-                raise RuntimeError("async-generator didn't yield") from None
+                raise RuntimeError("async-generator didn't stop") from None
         else:
             if exc_value is None:
                 exc_value = exc_type()
             try:
-                await self.agen.athrow(exc_type, exc_value, tb)
+                await self._agen.athrow(exc_type, exc_value, tb)
                 raise RuntimeError("async-generator didn't stop after athrow()")
             except StopAsyncIteration as exc:
                 return exc is not exc_value
@@ -49,9 +92,34 @@ class AsyncGenContextManager:
 def async_ctx_manager(func):
     @functools.wraps(func)
     def helper(*args, **kwargs):
-        return AsyncGenContextManager(func, args, kwargs)
+        return AsyncContextManager(func, args, kwargs)
     return helper
 
 
-# Shorter alias
+class AsyncContextGroup:
+
+    def __init__(self,
+                 context_managers: Optional[Iterable[AsyncContextManager]]=None):
+        self._cm = list(context_managers) if context_managers else []
+        self._cm_yields = []
+
+    def add(self, cm):
+        self._cm.append(cm)
+
+    async def __aenter__(self):
+        self._cm_yields[:] = await asyncio.gather(*(e.__aenter__() for e in self._cm))
+        return self._cm_yields
+
+    def contexts(self):
+        return self._cm_yields
+
+    async def __aexit__(self, *exc_info):
+        # TODO: improve exception handling
+        #  - should all ctxmgrs receive the same exception info?
+        await asyncio.gather(*(e.__aexit__(*exc_info) for e in self._cm))
+
+
+# Shorter aliases
 actxmgr = async_ctx_manager
+actxdecorator = AsyncContextDecorator
+actxgroup = AsyncContextGroup
--- tests/test_context.py
+++ tests/test_context.py
@@ -2,6 +2,30 @@ import pytest
 
 import asyncio
 import aiotools
+from aiotools.context import AbstractAsyncContextManager
+
+
+def test_actxmgr_types():
+
+    assert issubclass(aiotools.AsyncContextManager, AbstractAsyncContextManager)
+
+    class boilerplate_ctx():
+        async def __aenter__(self):
+            return self
+
+        async def __aexit__(self, *exc_info):
+            return None
+
+    @aiotools.actxmgr
+    async def simple_ctx():
+        yield
+
+    async def simple_agen():
+        yield
+
+    assert issubclass(boilerplate_ctx, AbstractAsyncContextManager)
+    assert isinstance(simple_ctx(), AbstractAsyncContextManager)
+    assert not isinstance(simple_agen(), AbstractAsyncContextManager)
 
 
 @pytest.mark.asyncio
@@ -47,6 +71,83 @@ async def test_actxmgr(event_loop):
 
 
 @pytest.mark.asyncio
+async def test_actxmgr_reuse(event_loop):
+
+    @aiotools.actxmgr
+    async def simple_ctx(msg):
+        yield msg
+
+    cm = simple_ctx('hello')
+
+    async with cm as msg:
+        assert msg == 'hello'
+
+    with pytest.raises(RuntimeError) as excinfo:
+        async with cm as msg:
+            assert msg == 'hello'
+        assert "didn't yield" in excinfo.value.args[0]
+
+    cm = cm._recreate_cm()
+
+    async with cm as msg:
+        assert msg == 'hello'
+
+
+@pytest.mark.asyncio
+async def test_actxmgr_error(event_loop):
+
+    @aiotools.actxmgr
+    async def simple_ctx(msg):
+        yield msg
+
+    with pytest.raises(RuntimeError) as excinfo:
+        async with simple_ctx('hello') as msg:
+            assert msg == 'hello'
+            raise RuntimeError('bomb1')
+        assert 'bomb1' == excinfo.value.args[0]
+
+    @aiotools.actxmgr
+    async def simple_ctx(msg):
+        try:
+            yield msg
+        finally:
+            raise ValueError('bomb2')
+
+    with pytest.raises(ValueError) as excinfo:
+        async with simple_ctx('hello') as msg:
+            assert msg == 'hello'
+            raise RuntimeError('bomb1')
+        assert 'bomb2' == excinfo.value.args[0]
+        assert isinstance(excinfo.value.__cause__, ValueError)
+        assert 'bomb1' == excinfo.value.__cause__.args[0]
+
+
+
+@pytest.mark.asyncio
+async def test_actxmgr_no_stop(event_loop):
+
+    @aiotools.actxmgr
+    async def simple_ctx(msg):
+        try:
+            yield msg
+        except:
+            pass
+        finally:
+            yield msg
+        yield msg
+
+    with pytest.raises(RuntimeError) as excinfo:
+        async with simple_ctx('hello') as msg:
+            assert msg == 'hello'
+        assert "didn't stop" in excinfo.value.args[0]
+
+    with pytest.raises(RuntimeError) as excinfo:
+        async with simple_ctx('hello') as msg:
+            raise ValueError('oops')
+        assert "didn't stop after athrow" in excinfo.value.args[0]
+
+
+@pytest.mark.asyncio
 async def test_actxmgr_no_yield(event_loop):
 
     @aiotools.actxmgr
@@ -58,10 +159,103 @@ async def test_actxmgr_no_yield(event_loop):
     def no_yield_ctx2(msg):
         pass
 
-    with pytest.raises(RuntimeError):
+    with pytest.raises(RuntimeError) as excinfo:
         async with no_yield_ctx1('hello') as msg:
             pass
+        assert "must be async-gen" in excinfo.value.args[0]
 
-    with pytest.raises(RuntimeError):
+    with pytest.raises(RuntimeError) as excinfo:
         async with no_yield_ctx2('hello') as msg:
             pass
+        assert "must be async-gen" in excinfo.value.args[0]
+
+
+@pytest.mark.asyncio
+async def test_actxdecorator(event_loop):
+
+    step = 0
+
+    class myacontext(aiotools.AsyncContextDecorator):
+
+        async def __aenter__(self):
+            nonlocal step
+            step = 1
+            return self
+
+        async def __aexit__(self, exc_type, exc, tb):
+            nonlocal step
+            step = 2
+            return False
+
+    # NOTE: don't forget the ending parenthesis which instantiates myacontext
+    @myacontext()
+    async def myfunc():
+        assert step == 1
+
+    assert step == 0
+    await myfunc()
+    assert step == 2
+
+    @myacontext()
+    async def myfunc():
+        assert step == 1
+        raise RuntimeError('oops')
+
+    step = 0
+    assert step == 0
+    try:
+        await myfunc()
+    except BaseException as e:
+        assert e.args[0] == 'oops'
+        assert step == 2
+    finally:
+        assert step == 2
+
+    @myacontext()
+    async def myfunc():
+        assert step == 1
+
+    # Use as a plain async context manager.
+    step = 0
+    assert step == 0
+    async with myacontext():
+        assert step == 1
+    assert step == 2
+
+
+@pytest.mark.asyncio
+async def test_actxgroup(event_loop):
+
+    # Test basic function.
+    exit_count = 0
+
+    @aiotools.actxmgr
+    async def ctx(a):
+        nonlocal exit_count
+        yield a + 10
+        exit_count += 1
+
+    ctxgrp = aiotools.actxgroup()
+
+    for i in range(3):
+        ctxgrp.add(ctx(i))
+
+    async with ctxgrp as values:
+        assert values[0] == 10
+        assert values[1] == 11
+        assert values[2] == 12
+        assert values == ctxgrp.contexts()
+
+    assert exit_count == 3
+
+    # Test generator/iterator initialization
+    exit_count = 0
+    ctxgrp = aiotools.actxgroup(ctx(i) for i in range(3))
+
+    async with ctxgrp as values:
+        assert values[0] == 10
+        assert values[1] == 11
+        assert values[2] == 12
+        assert values == ctxgrp.contexts()
+
+    assert exit_count == 3
