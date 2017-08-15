5b852d9f80134e48bbd4a257461e452f90270096
jaehyung.kwak <jaehyung.kwak@traport.com>
2017-08-15 15:46:38 +0900

    add Example contextgroup.py to test "aiotools.actxmgr" group gathering.

---
+++ examples/contextgroup.py
@@ -0,0 +1,26 @@
+from pprint import pprint
+
+import asyncio
+import aiotools
+
+@aiotools.actxmgr
+async def mygen(id):
+    yield f'mygen id is {id}'
+
+async def run():
+    ctxgrp = aiotools.actxgroup(mygen(i) for i in range(10))
+
+    async with ctxgrp as values:
+
+        pprint(values)
+
+if __name__ == '__main__':
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    try:
+        loop.run_until_complete(run())
+
+    except:
+        loop.close()
