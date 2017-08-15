c409a932bbb3225d062f5804951404f48a83f2ed
jaehyung.kwak <jaehyung.kwak@traport.com>
2017-08-15 14:55:07 +0900

    Add Example ticktock to test create_timer

---
+++ examples/ticktock.py
@@ -0,0 +1,25 @@
+import aiotools
+import asyncio
+
+async def mytick(interval):
+    print('tick')
+
+async def run():
+    t = aiotools.create_timer(mytick, 1.0)
+
+    await asyncio.sleep(4)
+
+    t.cancel()
+    await t
+
+if __name__ == '__main__':
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    try:
+        loop.run_until_complete(run())
+
+    except:
+        loop.close()
+
