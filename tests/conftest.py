4e7ef5cabba667175e494a40c4b722cea08cf01a
Seokju Hong <tjkj555@gmail.com>
2017-08-18 11:07:06 +0900

    Provide behaviors explicitly for every possible loop policy input

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,12 +10,15 @@ def pytest_collection_modifyitems(config, items):
     # TODO: We could skip some tests regarding the specific type of
     # event loop policy.
     loop_policy = config.getoption('--loop-policy')
-    if loop_policy == 'uvloop':
+    if loop_policy == 'default' or not loop_policy:
+        asyncio.set_event_loop_policy(None)
+    elif loop_policy == 'uvloop':
         import uvloop
         asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-        return
     elif loop_policy == 'tokio':
         # import tokio
         # asyncio.set_event_loop_policy(tokio.EventLoopPolicy())
         raise NotImplementedError('tokio module is not setup properly.')
-    return
+    else:
+        raise NotImplementedError(f'{loop_policy} loop policy is not supported '
+                                   'for now.')
