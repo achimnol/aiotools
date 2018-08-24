a5a23acb13b12dbdb0e53f9823375c17f19e9699
Joongi Kim <me@daybreaker.info>
2018-08-24 12:22:35 +0900

    repo: Restructure to use setup.cfg and the src directory
    * This removes all boilerplates in setup.py :)

---
+++ src/aiotools/__init__.py
@@ -0,0 +1,22 @@
+import pkg_resources
+try:
+    pkg_resources.get_distribution('aiotools')
+except pkg_resources.DistributionNotFound:
+    pass
+else:
+    # Import submodules only when installed properly
+    from .context import *  # noqa
+    from .func import *  # noqa
+    from .iter import *  # noqa
+    from .timer import *  # noqa
+    from .server import *  # noqa
+
+    __all__ = (
+        context.__all__,  # noqa
+        func.__all__,  # noqa
+        iter.__all__,  # noqa
+        timer.__all__,  # noqa
+        server.__all__,  # noqa
+    )
+
+__version__ = '0.6.1a1'
