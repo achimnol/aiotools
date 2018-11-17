353bf9580baba6fc273d0479c62200867fa90e3f
Joongi Kim <me@daybreaker.info>
2018-11-18 00:40:48 +0900

    docs: Fix ordering of module synopsis and function docs.

--- src/aiotools/server.py
+++ src/aiotools/server.py
@@ -2,6 +2,25 @@
 Based on :doc:`aiotools.context`, this module provides an automated lifecycle
 management for multi-process servers with explicit initialization steps and
 graceful shutdown steps.
+
+.. function:: server(func)
+
+    A decorator wrapper for :class:`AsyncServerContextManager`.
+
+    Usage example:
+
+    .. code:: python
+
+       @aiotools.server
+       async def myserver(loop, pidx, args):
+           await do_init(args)
+           stop_sig = yield
+           if stop_sig == signal.SIGINT:
+               await do_graceful_shutdown()
+           else:
+               await do_forced_shutdown()
+
+       aiotools.start_server(myserver, ...)
 '''
 
 import asyncio
