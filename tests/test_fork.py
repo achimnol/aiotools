a173b9a15cf9c46ade27cdb7e6a4dec0e5e29d90
Joongi Kim <me@daybreaker.info>
2021-01-10 00:48:58 +0900

    test(fork): Fix lint error and refactor duplicate codes

--- tests/test_fork.py
+++ tests/test_fork.py
@@ -1,6 +1,5 @@
 import asyncio
 import signal
-import sys
 import time
 from unittest import mock
 
@@ -10,12 +9,7 @@ from aiotools import fork as fork_mod
 from aiotools.fork import fork, PidfdChildProcess
 
 
-@pytest.mark.skipif(
-    not hasattr(signal, 'pidfd_send_signal'),
-    reason="pidfd is supported in Python 3.9+ and Linux kernel 5.4+"
-)
-@pytest.mark.asyncio
-async def test_fork():
+async def _do_test_fork():
 
     def child():
         print("hello world")
@@ -30,12 +24,7 @@ async def test_fork():
     assert ret == 99
 
 
-@pytest.mark.skipif(
-    not hasattr(signal, 'pidfd_send_signal'),
-    reason="pidfd is supported in Python 3.9+ and Linux kernel 5.4+"
-)
-@pytest.mark.asyncio
-async def test_fork_already_termination():
+async def _do_test_fork_already_terminated():
 
     def child():
         time.sleep(0.1)
@@ -50,12 +39,7 @@ async def test_fork_already_termination():
     assert ret == 99
 
 
-@pytest.mark.skipif(
-    not hasattr(signal, 'pidfd_send_signal'),
-    reason="pidfd is supported in Python 3.9+ and Linux kernel 5.4+"
-)
-@pytest.mark.asyncio
-async def test_fork_signal():
+async def _do_test_fork_signal():
 
     def child():
         try:
@@ -73,12 +57,7 @@ async def test_fork_signal():
     assert ret == 101
 
 
-@pytest.mark.skipif(
-    not hasattr(signal, 'pidfd_send_signal'),
-    reason="pidfd is supported in Python 3.9+ and Linux kernel 5.4+"
-)
-@pytest.mark.asyncio
-async def test_fork_many():
+async def _do_test_fork_many():
 
     def child():
         try:
@@ -105,20 +84,48 @@ async def test_fork_many():
         assert ret_list[i] == -15  # killed by SIGTERM
 
 
+@pytest.mark.skipif(
+    not hasattr(signal, 'pidfd_send_signal'),
+    reason="pidfd is supported in Python 3.9+ and Linux kernel 5.4+"
+)
+@pytest.mark.asyncio
+async def test_fork():
+    await _do_test_fork()
+
+
+@pytest.mark.skipif(
+    not hasattr(signal, 'pidfd_send_signal'),
+    reason="pidfd is supported in Python 3.9+ and Linux kernel 5.4+"
+)
+@pytest.mark.asyncio
+async def test_fork_already_terminated():
+    await _do_test_fork_already_terminated()
+
+
+@pytest.mark.skipif(
+    not hasattr(signal, 'pidfd_send_signal'),
+    reason="pidfd is supported in Python 3.9+ and Linux kernel 5.4+"
+)
+@pytest.mark.asyncio
+async def test_fork_signal():
+    await _do_test_fork_signal()
+
+
+@pytest.mark.skipif(
+    not hasattr(signal, 'pidfd_send_signal'),
+    reason="pidfd is supported in Python 3.9+ and Linux kernel 5.4+"
+)
+@pytest.mark.asyncio
+async def test_fork_many():
+    await _do_test_fork_many()
+
+
 @pytest.mark.asyncio
 async def test_fork_fallback():
     with mock.patch.object(
         fork_mod, '_has_pidfd', False,
     ):
-        def child():
-            print("hello world")
-            time.sleep(0.1)
-            return 99
-
-        proc = await fork(child)
-        assert proc._pid > 0
-        ret = await proc.wait()
-        assert ret == 99
+        await _do_test_fork()
 
 
 @pytest.mark.asyncio
@@ -126,15 +133,7 @@ async def test_fork_already_termination_fallback():
     with mock.patch.object(
         fork_mod, '_has_pidfd', False,
     ):
-        def child():
-            time.sleep(0.1)
-            return 99
-
-        proc = await fork(child)
-        assert proc._pid > 0
-        await asyncio.sleep(0.5)
-        ret = await proc.wait()
-        assert ret == 99
+        await _do_test_fork_already_terminated()
 
 
 @pytest.mark.asyncio
@@ -142,18 +141,7 @@ async def test_fork_signal_fallback():
     with mock.patch.object(
         fork_mod, '_has_pidfd', False,
     ):
-        def child():
-            try:
-                time.sleep(10)
-            except KeyboardInterrupt:
-                return 101
-            return 100
-
-        proc = await fork(child)
-        assert proc._pid > 0
-        proc.send_signal(signal.SIGINT)
-        ret = await proc.wait()
-        assert ret == 101
+        await _do_test_fork_signal()
 
 
 @pytest.mark.asyncio
@@ -161,26 +149,4 @@ async def test_fork_many_fallback():
     with mock.patch.object(
         fork_mod, '_has_pidfd', False,
     ):
-        def child():
-            try:
-                time.sleep(10)
-            except KeyboardInterrupt:
-                return 101
-            return 100
-
-        proc_list = []
-        for _ in range(32):
-            proc = await fork(child)
-            proc_list.append(proc)
-            assert proc._pid > 0
-            if isinstance(proc, PidfdChildProcess):
-                assert proc._pidfd > 0
-        for i in range(16):
-            proc_list[i].send_signal(signal.SIGINT)
-        for i in range(16, 32):
-            proc_list[i].send_signal(signal.SIGTERM)
-        ret_list = await asyncio.gather(*[proc.wait() for proc in proc_list])
-        for i in range(16):
-            assert ret_list[i] == 101
-        for i in range(16, 32):
-            assert ret_list[i] == -15  # killed by SIGTERM
+        await _do_test_fork_many()
