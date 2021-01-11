c402093238ebf618b4d7a481b2c42a91ed748b9e
Joongi Kim <me@daybreaker.info>
2021-01-12 01:12:30 +0900

    docs: Update the author information to include contributors

--- docs/conf.py
+++ docs/conf.py
@@ -63,8 +63,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'aiotools'
-copyright = '2018-2021, Joongi Kim'
-author = 'Joongi Kim'
+copyright = '2018-2021, Joongi Kim and contributors'
+author = 'Joongi Kim and contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
