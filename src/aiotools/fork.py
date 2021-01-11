634fd18b8bc8af2e2a9f8c29f812f8a5df5a3145
Joongi Kim <me@daybreaker.info>
2021-01-12 00:43:58 +0900

    docs: Add docs for AbstractChildProcess methods

--- src/aiotools/fork.py
+++ src/aiotools/fork.py
@@ -60,10 +60,21 @@ class AbstractChildProcess(metaclass=ABCMeta):
 
     @abstractmethod
     def send_signal(self, signum: int) -> None:
+        """
+        Send a UNIX signal to the child process.
+        If the child process is already terminated, it will log
+        a warning message and return.
+        """
         raise NotImplementedError
 
     @abstractmethod
     async def wait(self) -> int:
+        """
+        Wait until the child process terminates or reclaim the child process' exit
+        code if already terminated.
+        If there are other coroutines that has waited the same process, it may
+        return 255 and log a warning message.
+        """
         raise NotImplementedError
 
 
