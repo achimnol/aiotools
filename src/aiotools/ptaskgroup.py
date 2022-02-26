69f3b6ccfda4fbf9265bbb5af3bc0d5b6534439a
Joongi Kim <joongi@lablup.com>
2022-02-26 14:57:04 +0000

    refactor: Rewrite PersistentTaskGroup
    * Refer asyncio.TaskGroup to handle multiple cancellation correctly.
    * Make it explicit that:
      - "async with" stays with the original TaskGroup semantics.
      - Demonstrate usage without "async with" for long-lived objects
        for server applications.
      - "shutdown()" allows abortion in the middle, even from another
        parent task.
      - Custom exception handlers are also treated as child tasks.
        (TODO: what if exceptions occur in exception handlers?)

--- src/aiotools/ptaskgroup.py
+++ src/aiotools/ptaskgroup.py
@@ -8,6 +8,8 @@ from types import TracebackType
 from typing import (
     Any,
     Coroutine,
+    Generic,
+    List,
     Optional,
     Type,
     TypeVar,
@@ -25,7 +27,7 @@ __all__ = (
     'PersistentTaskGroup',
 )
 
-TAny = TypeVar('TAny')
+TResult = TypeVar('TResult')
 
 _ptaskgroup_idx = itertools.count()
 _log = logging.getLogger(__name__)
@@ -40,18 +42,32 @@ UNDEFINED = UndefinedResult.UNDEFINED
 
 
 class ExceptionHandler(Protocol):
-    async def __call__(self, exc: Exception) -> None:
+    async def __call__(self, exc: BaseException) -> None:
         ...
 
 
-async def _default_exc_handler(exc: Exception) -> None:
+async def _default_exc_handler(exc: BaseException) -> None:
     traceback.print_exc()
 
 
-class PersistentTaskGroup:
+class PersistentTaskGroup(Generic[TResult]):
+    """
+    Provides an abstraction of long-running task group for server applications.
 
+    If used as async context manager, it propagates cancellation from the parent task
+    into the child tasks.
+    It exits the context scope when all tasks finish, just like
+    :class:`asyncio.TaskGroup`.
+
+    If used without async context manager, it keeps running until
+    :method:`shutdown()` is called explicitly.
+    """
+
+    _base_error: Optional[BaseException]
     _exc_handler: ExceptionHandler
-    _tasks: "weakref.WeakSet[asyncio.Task[Any]]"
+    _errors: Optional[List[BaseException]]
+    _tasks: "weakref.WeakSet[asyncio.Task[TResult]]"
+    _on_completed_fut: Optional[asyncio.Future]
 
     def __init__(
         self,
@@ -59,7 +75,16 @@ class PersistentTaskGroup:
         name: str = None,
         exception_handler: ExceptionHandler = None,
     ) -> None:
+        self._entered = False
+        self._exiting = False
+        self._aborting = False
+        self._errors = []
+        self._base_error = None
         self._name = name or f"PTaskGroup-{next(_ptaskgroup_idx)}"
+        self._parent_cancel_requested = False
+        self._unfinished_tasks = 0
+        self._on_completed_fut = None
+        self._parent_task = compat.current_task()
         self._tasks = weakref.WeakSet()
         if exception_handler is None:
             self._exc_handler = _default_exc_handler
@@ -72,54 +97,109 @@ class PersistentTaskGroup:
 
     def create_task(
         self,
-        coro: Coroutine[Any, Any, TAny],
+        coro: Coroutine[Any, Any, TResult],
         *,
         name: str = None,
-    ) -> "asyncio.Task[Union[TAny, UndefinedResult]]":
-
-        # TODO: functools.wraps equivalent for coro?
-        async def wrapped_task() -> Union[TAny, UndefinedResult]:
-            current_task = compat.current_task()
-            assert current_task is not None
-            _log.debug("%r is spawned in %r.", current_task, self)
-            try:
-                return await coro
-            except asyncio.CancelledError:
-                _log.debug("%r in %r has been cancelled.", current_task, self)
-            except Exception as exc:
-                await self._exc_handler(exc)
-            except BaseException:
-                # TODO: implement
-                raise
-            # As our fallback handler handled the exception, the task should
-            # terminate silently with no explicit result.
-            # TODO: Add support for ExceptionGroup in Python 3.11, for the cases
-            #       with nested sub-tasks and sub-taskgroups.
-            return UNDEFINED
-
+    ) -> "asyncio.Task[TResult]":
         loop = compat.get_running_loop()
         if _has_task_name:
-            t = loop.create_task(wrapped_task(), name=name)
+            t = loop.create_task(coro, name=name)
         else:
-            t = loop.create_task(wrapped_task())
+            t = loop.create_task(coro)
+        _log.debug("%r is spawned in %r.", t, self)
+        self._unfinished_tasks += 1
+        t.add_done_callback(self._on_task_done)
         self._tasks.add(t)
         return t
 
-    async def shutdown(self) -> None:
-        remaining_tasks = {*self._tasks}
-        cancelled_tasks = set()
-        for t in remaining_tasks:
-            if t.cancelled():
-                continue
+    def _is_base_error(self, exc: BaseException) -> bool:
+        assert isinstance(exc, BaseException)
+        return isinstance(exc, (SystemExit, KeyboardInterrupt))
+
+    async def _wait_completion(self) -> Optional[BaseException]:
+        loop = compat.get_running_loop()
+        propagate_cancellation_error = None
+        while self._unfinished_tasks:
+            if self._on_completed_fut is None:
+                self._on_completed_fut = loop.create_future()
+            try:
+                await self._on_completed_fut
+            except asyncio.CancelledError as ex:
+                if not self._aborting:
+                    propagate_cancellation_error = ex
+                    self._trigger_shutdown()
+            self._on_completed_fut = None
+
+        assert self._unfinished_tasks == 0
+        self._on_completed_fut = None
+        return propagate_cancellation_error
+
+    def _trigger_shutdown(self) -> None:
+        self._aborting = True
+        for t in self._tasks:
             if not t.done():
                 t.cancel()
-                cancelled_tasks.add(t)
-        # Even though we handle CancelledError in wrapped_task,
-        # there are still possibilities to raise CancelledError
-        # when the tasks are not scheduled yet.
-        await asyncio.gather(*cancelled_tasks, return_exceptions=True)
+
+    async def shutdown(self) -> None:
+        self._trigger_shutdown()
+        await self._wait_completion()
+
+    def _on_exc_handler_done(self, task: asyncio.Task[None]) -> None:
+        self._unfinished_tasks -= 1
+        assert self._unfinished_tasks >= 0
+
+        # TODO: how to handle exceptions in the exception handler?
+        # TODO: prevent infinite recursion
+
+        if self._on_completed_fut is not None and not self._unfinished_tasks:
+            if not self._on_completed_fut.done():
+                self._on_completed_fut.set_result(True)
+
+    def _on_task_done(self, task: asyncio.Task[TResult]) -> None:
+        self._unfinished_tasks -= 1
+        assert self._unfinished_tasks >= 0
+        assert self._parent_task is not None
+        assert self._errors is not None
+        set_completed = False
+
+        if self._on_completed_fut is not None and not self._unfinished_tasks:
+            if not self._on_completed_fut.done():
+                set_completed = True
+
+        try:
+            if task.cancelled():
+                _log.debug("%r in %r has been cancelled.", task, self)
+                return
+
+            exc = task.exception()
+            if exc is None:
+                return
+
+            # Spawn our exception handler for non-base exceptions
+            # and swallow the error.
+            if not self._is_base_error(exc):
+                t = asyncio.create_task(self._exc_handler(exc))
+                t.add_done_callback(self._on_exc_handler_done)
+                self._tasks.add(t)
+                self._unfinished_tasks += 1
+                set_completed = False
+                return
+
+            # Now the exception is BaseException.
+            self._errors.append(exc)
+            if self._base_error is None:
+                self._base_error = exc
+
+            self._trigger_shutdown()
+            if not self._parent_task.cancelling():
+                self._parent_cancel_requested = True
+        finally:
+            if self._on_completed_fut is not None and set_completed:
+                self._on_completed_fut.set_result(True)
 
     async def __aenter__(self) -> "PersistentTaskGroup":
+        self._parent_task = compat.current_task()
+        self._entered = True
         return self
 
     async def __aexit__(
@@ -128,8 +208,60 @@ class PersistentTaskGroup:
         exc_val: Optional[BaseException],
         exc_tb: Optional[TracebackType],
     ) -> Optional[bool]:
-        await self.shutdown()
-        return False
+        self._exiting = True
+        assert self._errors is not None
+        propagate_cancellation_error: Optional[
+            Union[Type[BaseException], BaseException]
+        ] = None
+
+        if (exc_val is not None and
+                self._is_base_error(exc_val) and
+                self._base_error is None):
+            self._base_error = exc_val
+
+        if exc_type is asyncio.CancelledError:
+            if self._parent_cancel_requested:
+                self._parent_task.uncancel()
+            else:
+                propagate_cancellation_error = exc_type
+        if exc_type is not None and not self._aborting:
+            if exc_type is asyncio.CancelledError:
+                propagate_cancellation_error = exc_type
+            self._trigger_shutdown()
+
+        prop_ex = await self._wait_completion()
+        if prop_ex is not None:
+            propagate_cancellation_error = prop_ex
+        if propagate_cancellation_error is not None:
+            raise propagate_cancellation_error
+
+        if exc_type is not None and exc_type is not asyncio.CancelledError:
+            # If there are any unhandled errors, let's add them to
+            # the bubbled up exception group.
+            # Normally, they should have been swallowed and logged
+            # by the fallback exception handler.
+            self._errors.append(exc_val)
+
+        if self._errors:
+            # Bubble up errors
+            errors = self._errors
+            self._errors = None
+            me = BaseExceptionGroup('unhandled errors in a TaskGroup', errors)
+            raise me from None
+
+        return None
 
     def __repr__(self) -> str:
-        return f"<PersistentTaskGroup {self.name}>"
+        info = ['']
+        if self._tasks:
+            info.append(f'tasks={len(self._tasks)}')
+        if self._unfinished_tasks:
+            info.append(f'unfinished={self._unfinished_tasks}')
+        if self._errors:
+            info.append(f'errors={len(self._errors)}')
+        if self._aborting:
+            info.append('cancelling')
+        elif self._entered:
+            info.append('entered')
+        info_str = ' '.join(info)
+        return f'<PersistentTaskGroup{info_str}>'
--- tests/test_ptaskgroup.py
+++ tests/test_ptaskgroup.py
@@ -10,13 +10,13 @@ async def test_ptaskgroup_all_done():
 
     count = 0
 
+    async def subtask():
+        nonlocal count
+        await asyncio.sleep(0.1)
+        count += 1
+
     vclock = aiotools.VirtualClock()
     with vclock.patch_loop():
-
-        async def subtask():
-            nonlocal count
-            await asyncio.sleep(0.1)
-            count += 1
 
         async with aiotools.PersistentTaskGroup() as tg:
             assert tg.name.startswith("PTaskGroup-")
@@ -24,7 +24,7 @@ async def test_ptaskgroup_all_done():
                 t = tg.create_task(subtask(), name=f"Task-{idx}")
                 if sys.version_info >= (3, 8):
                     assert t.get_name() == f"Task-{idx}"
-                del t  # to prevent ref-leak after loop
+                del t  # prevent ref-leak after loop
             assert len(tg._tasks) == 10
             # all done
             await asyncio.sleep(0.2)
@@ -32,6 +32,88 @@ async def test_ptaskgroup_all_done():
             assert len(tg._tasks) == 0
 
         assert count == 10
+
+
+@pytest.mark.asyncio
+async def test_ptaskgroup_as_obj_attr():
+
+    count = 0
+
+    async def subtask():
+        nonlocal count
+        await asyncio.sleep(0.1)
+        count += 1
+
+    class LongLivedObject:
+
+        def __init__(self):
+            self.tg = aiotools.PersistentTaskGroup()
+
+        async def work(self):
+            self.tg.create_task(subtask())
+
+        async def aclose(self):
+            await self.tg.shutdown()
+
+    vclock = aiotools.VirtualClock()
+    with vclock.patch_loop():
+
+        obj = LongLivedObject()
+        for idx in range(10):
+            await obj.work()
+        assert len(obj.tg._tasks) == 10
+
+        # shutdown after all done
+        await asyncio.sleep(0.2)
+        await obj.aclose()
+
+        assert count == 10
+        assert len(obj.tg._tasks) == 0
+
+        count = 0
+        obj = LongLivedObject()
+        for idx in range(10):
+            await obj.work()
+        assert len(obj.tg._tasks) == 10
+
+        # shutdown immediately
+        await obj.aclose()
+
+        assert count == 0
+        assert len(obj.tg._tasks) == 0
+
+
+@pytest.mark.asyncio
+async def test_ptaskgroup_shutdown_from_different_task():
+
+    count = 0
+
+    async def subtask(idx):
+        nonlocal count
+        await asyncio.sleep(0.1 * idx)
+        count += 1
+
+    vclock = aiotools.VirtualClock()
+    with vclock.patch_loop():
+
+        tg = aiotools.PersistentTaskGroup()
+
+        async def _main_task():
+            async with tg:
+                for idx in range(10):
+                    tg.create_task(subtask(idx))
+                assert len(tg._tasks) == 10
+
+        async def _canceller_task():
+            await asyncio.sleep(0.49)
+            await tg.shutdown()
+
+        async with asyncio.TaskGroup() as outer_tg:
+            outer_tg.create_task(_main_task())
+            outer_tg.create_task(_canceller_task())
+
+        assert count == 5
+        assert len(tg._tasks) == 0
 
 
 @pytest.mark.asyncio
@@ -52,12 +134,11 @@ async def test_ptaskgroup_cancel_after_schedule():
                 tg.create_task(subtask())
             await asyncio.sleep(0)
             assert len(tg._tasks) == 10
-            # all cancelled after scheduled
 
-        assert count == 0
-        assert len(tg._tasks) == 10
-        await asyncio.sleep(0)
-        # after cancellation, all refs should be gone
+        # shutdown after exit (all done) is no-op.
+        assert count == 10
+        assert len(tg._tasks) == 0
+        await tg.shutdown()
         assert len(tg._tasks) == 0
 
 
@@ -78,12 +159,10 @@ async def test_ptaskgroup_cancel_before_schedule():
             for _ in range(10):
                 tg.create_task(subtask())
             assert len(tg._tasks) == 10
-            # all cancelled before scheduled
+            # let's abort immediately.
+            await tg.shutdown()
 
         assert count == 0
-        assert len(tg._tasks) == 10
-        await asyncio.sleep(0)
-        # after cancellation, all refs should be gone
         assert len(tg._tasks) == 0
 
 
@@ -93,19 +172,19 @@ async def test_ptaskgroup_exc_handler():
     count = 0
     error_count = 0
 
+    async def subtask():
+        nonlocal count
+        await asyncio.sleep(0.1)
+        1 / 0
+        count += 1
+
+    async def handler(e):
+        nonlocal error_count
+        assert isinstance(e, ZeroDivisionError)
+        error_count += 1
+
     vclock = aiotools.VirtualClock()
     with vclock.patch_loop():
-
-        async def subtask():
-            nonlocal count
-            await asyncio.sleep(0.1)
-            1 / 0
-            count += 1
-
-        async def handler(e):
-            nonlocal error_count
-            assert isinstance(e, ZeroDivisionError)
-            error_count += 1
 
         async with aiotools.PersistentTaskGroup(exception_handler=handler) as tg:
             for _ in range(10):
@@ -115,7 +194,6 @@ async def test_ptaskgroup_exc_handler():
 
         assert count == 0
         assert error_count == 10
-        # after handling error, all refs should be gone
         assert len(tg._tasks) == 0
 
 
@@ -124,27 +202,26 @@ async def test_ptaskgroup_cancel_with_await():
 
     count = 0
 
+    async def subtask():
+        nonlocal count
+        try:
+            await asyncio.sleep(0.1)
+            count += 1   # should not be executed
+        except asyncio.CancelledError:
+            await asyncio.sleep(0.1)
+            count += 10  # should be executed
+
     vclock = aiotools.VirtualClock()
     with vclock.patch_loop():
-
-        async def subtask():
-            nonlocal count
-            try:
-                await asyncio.sleep(0.1)
-                count += 1   # should not be executed
-            except asyncio.CancelledError:
-                await asyncio.sleep(0.1)
-                count += 10  # should be executed
 
         async with aiotools.PersistentTaskGroup() as tg:
             for _ in range(10):
                 tg.create_task(subtask())
             assert len(tg._tasks) == 10
-            # close it immediately after starting subtasks
-            await asyncio.sleep(0)
+            # shutdown just after starting child tasks
+            await asyncio.sleep(0.01)
+            await tg.shutdown()
 
         # ensure that awaits in all cancellation handling blocks have been executed
         assert count == 100
-        # after handling error, all refs should be gone
-        await asyncio.sleep(0)
         assert len(tg._tasks) == 0
