bf8f9dc6d6addb2da92df14b84cc4bd8f66fa5c7
Joongi Kim <joongi@lablup.com>
2022-03-06 08:09:53 +0000

    refactor: Prefer the official path

--- src/aiotools/taskgroup/__init__.py
+++ src/aiotools/taskgroup/__init__.py
@@ -2,17 +2,7 @@ import asyncio
 
 from .types import TaskGroupError
 
-if not hasattr(asyncio, 'TaskGroup'):
-    from . import base_compat
-    from . import persistent_compat
-    from .base_compat import *        # noqa
-    from .persistent_compat import *  # noqa
-    __all__ = (
-        'TaskGroupError',
-        *base_compat.__all__,
-        *persistent_compat.__all__,
-    )
-else:
+if hasattr(asyncio, 'TaskGroup'):
     from . import base
     from . import persistent
     from .base import *        # noqa
@@ -21,4 +11,14 @@ else:
         'TaskGroupError',
         *base.__all__,
         *persistent.__all__,
+    )
+else:
+    from . import base_compat
+    from . import persistent_compat
+    from .base_compat import *        # type: ignore  # noqa
+    from .persistent_compat import *  # type: ignore  # noqa
+    __all__ = (  # type: ignore
+        'TaskGroupError',
+        *base_compat.__all__,
+        *persistent_compat.__all__,
     )
