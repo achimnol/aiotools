f65cc97d875bdb57b309f52ffb66d5d1b46c30b8
Joongi Kim <joongi@lablup.com>
2022-03-07 01:06:52 +0900

    fix: Restore default export of MultiError (#40)

--- src/aiotools/taskgroup/__init__.py
+++ src/aiotools/taskgroup/__init__.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from .types import TaskGroupError
+from .types import MultiError, TaskGroupError
 
 if hasattr(asyncio, 'TaskGroup'):
     from . import base
@@ -8,6 +8,7 @@ if hasattr(asyncio, 'TaskGroup'):
     from .base import *        # noqa
     from .persistent import *  # noqa
     __all__ = (
+        'MultiError',
         'TaskGroupError',
         *base.__all__,
         *persistent.__all__,
@@ -18,6 +19,7 @@ else:
     from .base_compat import *        # type: ignore  # noqa
     from .persistent_compat import *  # type: ignore  # noqa
     __all__ = (  # type: ignore
+        'MultiError',
         'TaskGroupError',
         *base_compat.__all__,
         *persistent_compat.__all__,
