3848def4693e40a0187ceca030c6f5f9d8d70cf8
Jeong YunWon <69878+youknowone@users.noreply.github.com>
2022-03-28 16:21:42 +0900

    Allow import from windows environment (#47)

--- src/aiotools/__init__.py
+++ src/aiotools/__init__.py
@@ -1,29 +1,29 @@
+import sys
+import pkgutil
+
 # Import submodules only when installed properly
 from . import (
     context,
     defer as _defer,
-    fork as _fork,
     func,
     iter as _iter,
-    server,
     taskgroup,
     timer,
 )
 
-import pkgutil
-_version_data = pkgutil.get_data("aiotools", "VERSION")
-if _version_data is None:
-    __version__ = '0.0.dev'
-else:
-    __version__ = _version_data.decode('utf8').strip()
+_is_linux = sys.platform.startswith('linux')
+
+if _is_linux:
+    from . import (
+        fork as _fork,
+        server,
+    )
 
 __all__ = (
     *context.__all__,
     *_defer.__all__,
-    *_fork.__all__,
     *func.__all__,
     *_iter.__all__,
-    *server.__all__,
     *taskgroup.__all__,
     *timer.__all__,
     '__version__',
@@ -31,9 +31,24 @@ __all__ = (
 
 from .context import *     # noqa
 from .defer import *       # noqa
-from .fork import *        # noqa
 from .func import *        # noqa
 from .iter import *        # noqa
 from .taskgroup import *   # noqa
 from .timer import *       # noqa
-from .server import *      # noqa
+
+if _is_linux:
+    __all__ = (
+        *__all__,
+        *_fork.__all__,
+        *server.__all__,
+    )
+
+    from .fork import *        # noqa
+    from .server import *      # noqa
+
+
+_version_data = pkgutil.get_data("aiotools", "VERSION")
+if _version_data is None:
+    __version__ = '0.0.dev'
+else:
+    __version__ = _version_data.decode('utf8').strip()
