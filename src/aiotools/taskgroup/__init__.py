25d5e9830f89641e78c763915bab95e8af7911c6
Joongi Kim <me@daybreaker.info>
2025-01-22 22:41:50 +0900

    fix: Make public exports to be static checker friendly

--- src/aiotools/taskgroup/__init__.py
+++ src/aiotools/taskgroup/__init__.py
@@ -2,34 +2,26 @@ import asyncio
 
 from .types import MultiError, TaskGroupError
 
+__all__ = [
+    "MultiError",
+    "TaskGroup",
+    "TaskGroupError",
+    "PersistentTaskGroup",
+    "as_completed_safe",
+    "persistent",
+    "persistent_compat",
+    "current_taskgroup",
+    "has_contextvars",
+]
+
 if hasattr(asyncio, "TaskGroup"):
     from . import persistent
     from .base import *  # noqa
     from .persistent import *  # noqa
-
-    __all__ = [
-        "MultiError",
-        "TaskGroup",
-        "TaskGroupError",
-        "current_taskgroup",
-        *persistent.__all__,
-    ]
 else:
     from . import persistent_compat
     from .base_compat import *  # type: ignore  # noqa
     from .base_compat import has_contextvars
     from .persistent_compat import *  # type: ignore  # noqa
 
-    __all__ = [  # type: ignore  # noqa
-        "MultiError",
-        "TaskGroup",
-        "TaskGroupError",
-        *persistent_compat.__all__,
-    ]
-    if has_contextvars:
-        __all__.append("current_taskgroup")
-
-
 from .utils import as_completed_safe  # noqa
-
-__all__.append("as_completed_safe")
